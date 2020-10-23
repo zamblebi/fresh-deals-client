0e569c7b66a2cce2061e9f410914660fef063fbc
Samuel Bouye <bouyesamuel@gmail.com>
2020-10-23 18:26:45 +0000

    carousel slide works

--- src/components/Carousel/Courasel.jsx
+++ src/components/Carousel/Courasel.jsx
@@ -1,16 +1,62 @@
 import React, { useState } from 'react'
-
-const Carousel = () => {
+import dataSlide from './DataSlider'
+import './Slide.css'
+const Carousel = ({props}) => {
     
-    const [activeIndex, setActiveIndex] = useState({one: "Hello world"});
+    const [activeIndex, setActiveIndex] = useState(0);
+    // const [index, setIndex] = useState(dateSlide.length)
+    // let {dataSlide} = props;
+    
+    function goToSlide(index){
+        setActiveIndex(index)
+    }
+    
+    
+
+    const goToPrevSlide = (e) => {
+        e.preventDefault();
+        
+        let index = activeIndex;
+        let dataSlideLength = dataSlide.length -1;
+        
+        if(index < 1){
+            index = dataSlideLength; 
+        }else{
+            index--;
+        }
+        setActiveIndex(index)
+    }
+
+    const goToNextSlide = (e) => {
+        e.preventDefault();
+        
+        let index = activeIndex;
+        let dataSlideLength = dataSlide.length -1;
+
+        if(index == dataSlideLength){
+            // console.log(index == dataSlideLength)
+            index = 0 
+        }else{
+            index++
+        }
+        setActiveIndex(index)
 
 
+    }
 
-
+    console.log(activeIndex)
+    
     return (
-        // console.log
-        <div>
-            <h1>{activeIndex.one}</h1>
+        <div className="carousel">
+            <a href="#" className="carousel__arrow carousel___arrow--left" onClick={e => goToPrevSlide(e)}>Prev</a>
+                <ul className="carousel__slides">
+                   { dataSlide.map((slide,index) => (
+                       <li className={index == activeIndex ? 'carousel__slide carousel__slide--active' : 'carousel__slide--inactive'} key={index}>
+                            <p className="carousel__slide--content">{slide.title}</p>
+                        </li>
+                   ))}
+                </ul>
+            <a href="#" className="carousel__arrow carousel___arrow--right" onClick={e => goToNextSlide(e)}>Next</a>
         </div>
     )
 }
