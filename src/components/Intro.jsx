5033171a1a34931751fc70a8f78fb10c0d276d5f
Samuel Bouye <bouyesamuel@gmail.com>
2020-10-28 16:18:16 +0000

    new frontend page with tailwind components

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import './Intro.css'
+// import './Intro.css'
 import {Link} from 'react-router-dom'
 import Carousel from './Carousel/Courasel'
+import SomeArticle from './SomeArticle'
 const Intro = () => {
     return (
         <div className="intro">
                 <Carousel/>
-               
+                <SomeArticle/>
         </div>
     )
 }
